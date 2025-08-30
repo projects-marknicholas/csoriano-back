0bfc60b19b7e59ebe92c016a0909cfd431548c83
projects-marknicholas <prograzon@gmail.com>
2025-08-30 10:23:43 +0800

    remove the /

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
   // origin: ' http://localhost:5173', 
-  origin: 'https://csoriano.netlify.app/',
+  origin: 'https://csoriano.netlify.app',
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true, 
